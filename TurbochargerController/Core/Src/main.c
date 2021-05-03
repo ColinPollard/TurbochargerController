f7e743d17be7a3a59ed9a65324040c68459c0f61
lrm77 <you@example.com>
2021-05-02 18:48:17 -0600

    Updated ADC init

--- TurbochargerController/Core/Src/main.c
+++ TurbochargerController/Core/Src/main.c
@@ -63,7 +63,7 @@ void ADC_init(void) {
   //Configure ADC1 to 12-bit resolution, continuous conversion, hardware triggers disabled
   ADC1->CFGR |= (1<<13);
   ADC1->CFGR &= ~((1<<3)|(1<<4)|(1<<5)|(1<<10)|(1<<11));
-	
+
   //Enable Channel 12 in the ADC
   //ADC1->CHSELR |= 1 << 12;
   ADC1->DIFSEL &= ~(1 << 12);
@@ -75,13 +75,13 @@ void ADC_init(void) {
   //Set ADVREGEN to enable voltage regualtor start up
   ADC1->CR |= ADC_CR_ADVREGEN;
   HAL_Delay(1); // Wait for voltage regulator
-  
+
   //Calibrate the ADC
   //*****Clear ADEN
   if ((ADC1->CR & ADC_CR_ADEN) != 0) {
     ADC1->CR |= ADC_CR_ADDIS;
   }
-  while ((ADC1->CR & ADC_CR_ADEN) != 0){} 
+  while ((ADC1->CR & ADC_CR_ADEN) != 0){}
 
   //Set to single-ended input conversion
   ADC1->CR &= ~(1 << 30);
@@ -91,10 +91,10 @@ void ADC_init(void) {
 
   //*****Start Calibratiom
   ADC1->CR |= ADC_CR_ADCAL;
-
+ 
   //*****Wait until calibration is complete
   while ((ADC1->CR & ADC_CR_ADCAL) != 0) {}
-  
+ 
   //Enable/start the ADC
   //*****Clear ADRDY
   if ((ADC1->ISR & ADC_ISR_ADRDY) != 0) {
@@ -110,7 +110,7 @@ void ADC_init(void) {
   //Select channel 12 and select one conversion
   ADC1->SQR1 &= ~(0xF | (0x1F << 6));
   ADC1->SQR1 |= (12 << 6);
-
+  
   //*****Start the ADC
   ADC1->CR |= ADC_CR_ADSTART;
 }
